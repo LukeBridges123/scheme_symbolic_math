04a6d8d6c7bcc7aa57cfea7c53321ac905912031
Luke Bridges <lukebridges123@gmail.com>
2022-09-27 08:23:33 -0400

    Refactored 'deriv' function into smaller functions for each possible rule. Cleaned up the special function rules by using a case statement instead of a cond.

--- symbolic-differentiation.rkt
+++ symbolic-differentiation.rkt
@@ -9,57 +9,69 @@
                           0))
     ;(deriv (f + g)) = (deriv f) + (deriv g)
     ((sum? expr) (make-sum (deriv (addend expr) var) (deriv (augend expr) var)))
-    ;(deriv (f * g)) = (f * (deriv g)) + ((deriv f) * g))
-    ((product? expr) (make-sum
-                      (make-product (multiplier expr)
-                                    (deriv (multiplicand expr) var))
-                      (make-product (deriv (multiplier expr) var)
-                                    (multiplicand expr))))
-    ;(deriv (f / g)) = ((deriv f) * g) - (f * (deriv g)) / g^2
-    ((quotient? expr) (make-quotient
-                       (make-sum
-                        (make-product (deriv (numer expr) var) (denom expr))
-                        (make-product -1 (make-product (numer expr) (deriv (denom expr) var))))
-                       (make-exponentiation (denom expr) 2)))
-    ;(deriv (e ^ u)) = (e^u) * (deriv u)
-    ;
-    ((exponentiation? expr) (cond
-                              ((eq? (base expr) 'e) (make-product expr (deriv (power expr) var)))
-                              ((number? (base expr)) (make-product (make-product expr (make-special-function 'ln (base expr)))
-                                                                   (deriv (power expr) var)));
-                              ((number? (power expr))  
-                               (make-product
-                                (make-product (power expr)
-                                              (make-exponentiation (base expr) (- (power expr) 1)))
-                                (deriv (base expr) var)))
-                              (else (error "unknown expression type: DERIV" expr))))
-    
-    ((special-function? expr) (let ((outer-function (car expr))
-                                    (inner-function (cadr expr)))
-                                (cond
-                                  ((eq? outer-function 'sin) (make-product (make-special-function 'cos inner-function)
-                                                                           (deriv inner-function var)))
-                                  ((eq? outer-function 'cos) (make-product (make-product -1 (make-special-function 'sin inner-function))
-                                                                           (deriv inner-function var)))
-                                  ((eq? outer-function 'tan) (make-sum 1 (make-exponentiation (make-special-function 'tan var) 2)))
-                                  ((eq? outer-function 'ln) (make-quotient (deriv inner-function var)
-                                                                           inner-function))
-                                  ((eq? outer-function 'arcsin) (make-product (make-exponentiation (make-difference 1
-                                                                                                                    (make-exponentiation inner-function 2))
-                                                                                                   -1/2)
-                                                                              (deriv inner-function var)))
-                                  ((eq? outer-function 'arccos)
-                                   (make-product (make-product -1 (make-exponentiation (make-difference 1
-                                                                                                        (make-exponentiation inner-function 2))
-                                                                                       -1/2))
-                                                 (deriv inner-function var)))
-                                  ((eq? outer-function 'arctan) (make-product (make-quotient 1 (make-sum (make-exponentiation inner-function 2)
-                                                                                                         1))
-                                                                              (deriv inner-function var)))
-                                                                 
-                                  (else (error "unknown expression type: DERIV" expr)))))
-    
+    ((product? expr) (apply-product-rule (multiplier expr) (multiplicand expr) var))
+    ((quotient? expr) (apply-quotient-rule (numer expr) (denom expr) var))
+    ((exponentiation? expr) (apply-exponentiation-rules expr var))
+    ((special-function? expr) (apply-special-function-rules expr var))
     (else (error "unknown expression type: DERIV" expr))))
+
+;(deriv (u * v)) = (f * (deriv g)) + ((deriv f) * g))
+(define (apply-product-rule u v var)
+  (make-sum
+   (make-product (deriv u var) v)
+   (make-product u (deriv v var))))
+;(deriv (f / g)) = ((deriv f) * g) - (f * (deriv g)) / g^2
+(define (apply-quotient-rule u v var)
+  (make-quotient
+   (make-difference (make-product (deriv u var) v)
+                    (make-product u (deriv v var)))
+   (make-exponentiation v 2)))
+#|
+Handles the rules for many different types of exponentiation. When the base is e, leave the expression as is except for applying the chain rule.
+When the base is a number b, implicitly treat b^v as (e^(ln b))^v = e ^ (v * ln b). Applying the chain rule gets (e ^ (v * ln b)) * (ln b * (deriv v)) =
+b^v * ln b * deriv v.
+When the power is a number, apply the standard power rule and the chain rule, D(u^a) = a(u^(a-1)) * D(u).
+In the case where neither the base nor an exponent is a number or e (e.g. for x^x), just throw an error. Support for fully general exponentiations may be brought in
+later.
+|#
+(define (apply-exponentiation-rules expr var)
+  (cond
+    ((eq? (base expr) 'e) (make-product expr (deriv (power expr) var)))
+    ((number? (base expr)) (make-product (make-product expr (make-special-function 'ln (base expr)))
+                                         (deriv (power expr) var)));
+    ((number? (power expr))  
+     (make-product
+      (make-product (power expr)
+                    (make-exponentiation (base expr) (- (power expr) 1)))
+      (deriv (base expr) var)))
+    (else (error "unknown expression type: DERIV" expr))))
+;One big switch statement of all the currently-available possibilities for what a special function could be. Chain rule is used throughout.
+(define (apply-special-function-rules expr var)
+  (let ((outer-function (car expr))
+        (inner-function (cadr expr)))
+    (case outer-function
+      ('sin (make-product (make-special-function 'cos inner-function)
+                          (deriv inner-function var)))
+      ('cos (make-product (make-product -1 (make-special-function 'sin inner-function))
+                          (deriv inner-function var)))
+      ('tan (make-sum 1 (make-exponentiation (make-special-function 'tan var) 2)))
+      ('ln (make-quotient (deriv inner-function var)
+                          inner-function))
+      ('arcsin (make-product (make-exponentiation (make-difference 1
+                                                                   (make-exponentiation inner-function 2))
+                                                  -1/2)
+                             (deriv inner-function var)))
+      ('arccos
+       (make-product (make-product -1 (make-exponentiation (make-difference 1
+                                                                            (make-exponentiation inner-function 2))
+                                                           -1/2))
+                     (deriv inner-function var)))
+      ('arctan (make-product (make-quotient 1 (make-sum (make-exponentiation inner-function 2)
+                                                        1))
+                             (deriv inner-function var)))
+                                                                 
+      (else (error "unknown expression type: DERIV" expr)))))
+
 
 (define (taylor-series f var center num-of-terms)
   (define (taylor-series-iter nth-deriv degree-of-curr-term partial-taylor-series n-factorial)
@@ -68,7 +80,7 @@
                                      n-factorial))
                (curr-term (make-product coeff
                                         (make-exponentiation (make-difference var center)
-                                                                   degree-of-curr-term))))
+                                                             degree-of-curr-term))))
           (taylor-series-iter (deriv nth-deriv var)
                               (+ degree-of-curr-term 1)
                               (make-sum partial-taylor-series curr-term)
